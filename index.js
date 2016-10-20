f39668bb938eff0fa283eb3d6cac8b05d1c5ff0d
wojciech.sasiela <wojciech.sasiela@zeto.lublin.pl>
2016-10-20 13:58:22 +0200

    add simple table

--- index.js
+++ index.js
@@ -15,6 +15,47 @@ var user = {
   image: Faker.Image.avatar()
 };
 
+// http://adazzle.github.io/react-data-grid/examples.html#/basic
+var _rows = [];
+for (var i = 1; i < 1000; i++) {
+  _rows.push({
+    id: i,
+    title: 'Title ' + i,
+    count: i * 1000
+  });
+}
+
+//A rowGetter function is required by the grid to retrieve a row for a given index
+var rowGetter = function(i){
+  return _rows[i];
+};
+
+
+var columns = [
+{
+  key: 'id',
+  name: 'ID'
+},
+{
+  key: 'title',
+  name: 'Title'
+},
+{
+  key: 'count',
+  name: 'Count'
+}
+]
+
+var Example = React.createClass({
+  render: function() {
+    return  (<ReactDataGrid
+    columns={columns}
+    rowGetter={rowGetter}
+    rowsCount={_rows.length}
+    minHeight={500} />);
+  }
+});
+
 // Rapid case to fast learn Reactjs
 // https://toddmotto.com/react-create-class-versus-component/
 
@@ -94,7 +135,7 @@ class App extends React.Component {
 		<Timer />
 		<LikeCon />
         <Button />
-
+		<Example/>
       </div>
     );
   }
